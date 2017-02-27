1d57c000437638bf31f883a10b3646bb323d3081
giangnguyen95 <giangnt_58@vnu.edu.vn>
2017-02-27 09:34:48 +0700

    add background create by crazytalk animator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Task_LaravelDemo
+use open source: link: http://packalyst.com/packages/package/nahid/talk
+ link github: https://github.com/nahid/talk
--- talk-example/resources/views/context/images.blade.php
+++ talk-example/resources/views/context/images.blade.php
@@ -8,22 +8,22 @@
 	<ul class="list_img">
 		<li>
 			<a href="#">
-				<img src="{{asset('chat/images/Desert.jpg')}}" width="100px" height="100px">
+				<img src="{{asset('chat/images/bg_1.gif')}}" width="100px" height="100px">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="{{asset('chat/images/Hydrangeas.jpg')}}" width="100px" height="100px">
+				<img src="{{asset('chat/images/bg_2.gif')}}" width="100px" height="100px">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="{{asset('chat/images/Penguins.jpg')}}" width="100px" height="100px">
+				<img src="{{asset('chat/images/bg_3.gif')}}" width="100px" height="100px">
 			</a>
 		</li>
 		<li>
 			<a href="#">
-				<img src="{{asset('chat/images/Tulips.jpg')}}" width="100px" height="100px">
+				<img src="{{asset('chat/images/bg_4.gif')}}" width="100px" height="100px">
 			</a>
 		</li>
 	</ul>
